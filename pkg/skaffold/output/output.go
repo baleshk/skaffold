f4d20e178a835dc6bd603bf201ca0fd88c6aaffd
baleshk <balesh@google.com>
2021-08-25 12:53:35 +0530

    review comment fixed, remove commented code, serverMux error handller

--- pkg/skaffold/output/output.go
+++ pkg/skaffold/output/output.go
@@ -32,11 +32,11 @@ import (
 const timestampFormat = "2006-01-02 15:04:05"
 
 type skaffoldWriter struct {
-	MainWriter   io.Writer
-	EventWriter  io.Writer
-	EventWriter1 io.Writer
-	task         constants.Phase
-	subtask      string
+	MainWriter    io.Writer
+	EventWriter   io.Writer
+	EventWriterV3 io.Writer
+	task          constants.Phase
+	subtask       string
 
 	timestamps bool
 }
@@ -63,7 +63,7 @@ func (s skaffoldWriter) Write(p []byte) (int, error) {
 	written += n
 
 	s.EventWriter.Write(p)
-	s.EventWriter1.Write(p)
+	s.EventWriterV3.Write(p)
 
 	return written, nil
 }
@@ -74,10 +74,10 @@ func GetWriter(ctx context.Context, out io.Writer, defaultColor int, forceColors
 	}
 
 	return skaffoldWriter{
-		MainWriter:   SetupColors(out, defaultColor, forceColors),
-		EventWriter:  eventV2.NewLogger(constants.DevLoop, "-1"),
-		EventWriter1: eventV3.NewLogger(constants.DevLoop, "-1"),
-		timestamps:   timestamps,
+		MainWriter:    SetupColors(out, defaultColor, forceColors),
+		EventWriter:   eventV2.NewLogger(constants.DevLoop, "-1"),
+		EventWriterV3: eventV3.NewLogger(constants.DevLoop, "-1"),
+		timestamps:    timestamps,
 	}
 }
 
@@ -116,12 +116,12 @@ func WithEventContext(ctx context.Context, out io.Writer, phase constants.Phase,
 
 	if sw, isSW := out.(skaffoldWriter); isSW {
 		return skaffoldWriter{
-			MainWriter:   sw.MainWriter,
-			EventWriter:  eventV2.NewLogger(phase, subtaskID),
-			EventWriter1: eventV3.NewLogger(phase, subtaskID),
-			task:         phase,
-			subtask:      subtaskID,
-			timestamps:   sw.timestamps,
+			MainWriter:    sw.MainWriter,
+			EventWriter:   eventV2.NewLogger(phase, subtaskID),
+			EventWriterV3: eventV3.NewLogger(phase, subtaskID),
+			task:          phase,
+			subtask:       subtaskID,
+			timestamps:    sw.timestamps,
 		}
 	}
 
